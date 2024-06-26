d1cad6700ca0f66ed0a8ea57b33901b9b85520f2
jeff prince <jeffryaime@gmail.com>
2024-06-26 11:01:13 -0400

    shopping list

--- shopping-list-start/script.js
+++ shopping-list-start/script.js
@@ -1,6 +1,10 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearButton = document.getElementById('clear')
+const itemFilter = document.getElementById('filter')
+
+
 
 
 function addItem(e) {
@@ -20,7 +24,10 @@ function addItem(e) {
   const button = createButton('remove-item btn-link text-red')
   li.appendChild(button)
 
+  // Add li to the DOM
   itemList.appendChild(li);
+
+  checkUI();
 
   itemInput.value = '';
 
@@ -39,5 +46,40 @@ function createIcon(classes) {
   icon.className = classes;
   return icon;
 }
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove()
+
+      checkUI();
+    }
+  }
+}
+
+function clearItems() {
+  while(itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild)
+  }
+  checkUI();
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll('li')
+  if (items.length === 0) {
+    clearButton.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearButton.style.display = 'block';
+    itemFilter.style.display = 'block';
+
+  }
+}
+
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearButton.addEventListener('click', clearItems)
+
+checkUI();
