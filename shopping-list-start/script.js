cd3cb9a184ebdf91b4d7320ef7adbe5560f147a2
jeff prince <jeffryaime@gmail.com>
2024-06-26 16:23:30 -0400

    update

--- shopping-list-start/script.js
+++ shopping-list-start/script.js
@@ -3,6 +3,8 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear')
 const itemFilter = document.getElementById('filter')
+const formBtn = itemForm.querySelector('button')
+let isEditMode = false;
 
 function displayItems() {
   const itemsFromStorage = getItemsFromStorage();
@@ -82,21 +84,56 @@ function getItemsFromStorage() {
   return itemsFromStorage
 }
 
-
-function removeItem(e) {
+function onClickItem(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
-    if (confirm('Are you sure?')) {
-      e.target.parentElement.parentElement.remove()
-
-      checkUI();
-    }
+    removeItem(e.target.parentElement.parentElement);
+  } else {
+    setItemToEdit(e.target);
   }
+}
+
+function setItemToEdit(item) {
+  isEditMode = true;
+
+  itemList
+  .querySelectorAll('li')
+  .forEach((i) => i.classList.remove('edit-mode'));
+
+
+  item.classList.add('edit-mode');
+  formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+  formBtn.style.backgroundColor = '#228b22'
+  itemInput.value = item.textContent;
+}
+
+function removeItem(item) {
+  if (confirm('Are you sure?')) {
+    // Remove item from DOM
+    item.remove();
+
+    // Remove item from storage
+    removeItemFromStorage(item.textContent);
+
+    checkUI();
+  }
+}
+
+function removeItemFromStorage(item) {
+  let itemsFromStorage = getItemsFromStorage();
+
+  // Filter out item to be removed
+  itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
+
+  // Re-set to localstorage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
 }
 
 function clearItems() {
   while(itemList.firstChild) {
     itemList.removeChild(itemList.firstChild)
   }
+  // Clear from localStorage
+  localStorage.removeItem('items');
   checkUI();
 }
 
@@ -133,7 +170,7 @@ function checkUI() {
 function init() {
 // Event Listeners
 itemForm.addEventListener('submit', onAddItemSubmit);
-itemList.addEventListener('click', removeItem);
+itemList.addEventListener('click', onClickItem);
 clearButton.addEventListener('click', clearItems)
 itemFilter.addEventListener('input', filterItems)
 document.addEventListener('DOMContentLoaded', displayItems)
